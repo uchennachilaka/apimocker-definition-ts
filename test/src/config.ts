5a23d2a78968e61bfedf97396e479b5ed5a7ca55
Uche Chilaka <uche@lar.city>
2019-07-17 04:00:20 -0400

    (feature) fixed dynamic config

--- test/src/config.ts
+++ test/src/config.ts
@@ -1,21 +1,47 @@
 import * as path from 'path';
 
+enum Endpoints {
+  Friends = 'friends',
+  FriendById = 'friend/:userId',
+}
+
 // Config docs: https://www.npmjs.com/package/apimocker#configuration
-export default {
-  mockDirectory: path.resolve(__dirname, 'mocks'),
+module.exports = {
+  note: 'Friends API',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  mockDirectory: path.resolve(process.cwd(), 'test/mocks'),
   port: process.env.PORT || '7878',
   quiet: false,
-  latency: 50,
+  latency: '1000',
+  logRequestHeaders: true,
+  corsCredentials: true,
   webServices: {
-    friends: {
-      contentType: 'application/json',
+    [Endpoints.Friends]: {
       mockFile: 'forbidden.mock.json',
       verbs: ['get', 'post', 'put', 'delete'],
+      contentType: 'application/json',
       responses: {
         get: { httpStatus: 200, mockFile: 'friends.mock.json' },
         post: { httpStatus: 405, mockFile: 'forbidden.mock.json' },
         put: { httpStatus: 405, mockFile: 'forbidden.mock.json' },
         delete: { httpStatus: 405, mockFile: 'forbidden.mock.json' },
+      },
+    },
+    [Endpoints.FriendById]: {
+      mockFile: 'forbidden.mock.json',
+      verbs: ['get' /*, 'post', 'put', 'delete'*/],
+      contentType: 'application/json',
+      switch: ['userId'],
+      responses: {
+        // get: { httpStatus: 200, mockFile: 'friends.mock.json' },
+        post: { httpStatus: 405, mockFile: 'forbidden.mock.json' },
+        put: { httpStatus: 405, mockFile: 'forbidden.mock.json' },
+        delete: { httpStatus: 405, mockFile: 'forbidden.mock.json' },
+      },
+      jsonPathSwitchResponse: {
+        jsonpath: '$[?(@.userId==#userId#)]',
+        mockFile: 'friends.mock.json',
+        forceFirstObject: false,
       },
     },
   },
